fe121518e1429dfa41eea52c01d3110dbba5e9a5
Joeyray224 <45841303+joeyray224@users.noreply.github.com>
2020-02-12 22:20:42 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="script.js" defer></script>
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="header">
+        <div class="logo">
+          Кинотеатр "На коленке"
+        </div>
+        <div class="description">
+          <p>Начало сеансов: 10:00</p>
+          <p>Завершение сеансов: 20:00</p>
+        </div>
+        <div class="menu">
+          <a href="#">Кнопка меню</a>
+          <a href="#">Кнопка меню</a>
+          <a href="#">Кнопка меню</a>
+          <a href="#">Кнопка меню</a>
+        </div>
+    </div>
+    <div class="content">
+      <div class="calendar">
+      </div>
+    </div>
+    <div class="footer">
+      footer
+    </div>
+  </body>
+</html>
